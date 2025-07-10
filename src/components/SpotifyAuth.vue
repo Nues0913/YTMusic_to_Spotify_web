50e89fe9b397c3d04f562ec4597f15933b882aec
Nues0913 <106960429+Nues0913@users.noreply.github.com>
2025-07-10 14:09:15 +0800

    change crypto for build

--- src/components/SpotifyAuth.vue
+++ src/components/SpotifyAuth.vue
@@ -146,17 +146,12 @@ async function checkToken() {
 }
 
 async function authenticate() {
-  if (typeof window === 'undefined' || !window.crypto) {
-    console.error('Crypto not available during build.');
-    return;
-  }
-
   const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const randomValues = window.crypto.getRandomValues(new Uint8Array(64));
+  const randomValues = crypto.getRandomValues(new Uint8Array(64));
   const randomString = randomValues.reduce((acc, x) => acc + possible[x % possible.length], "");
   const code_verifier = randomString;
   const data = new TextEncoder().encode(code_verifier);
-  const hashed = await window.crypto.subtle.digest('SHA-256', data);
+  const hashed = await crypto.subtle.digest('SHA-256', data);
 
   const code_challenge_base64 = btoa(String.fromCharCode(...new Uint8Array(hashed)))
     .replace(/=/g, '')
