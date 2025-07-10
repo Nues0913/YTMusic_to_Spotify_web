68c6e06190708c8a25c8e96ebccc16a7589c58f7
Nues0913 <106960429+Nues0913@users.noreply.github.com>
2025-07-10 13:54:01 +0800

    change crypto for build

--- src/components/SpotifyAuth.vue
+++ src/components/SpotifyAuth.vue
@@ -152,11 +152,11 @@ async function authenticate() {
   }
 
   const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const randomValues = crypto.getRandomValues(new Uint8Array(64));
+  const randomValues = window.crypto.getRandomValues(new Uint8Array(64));
   const randomString = randomValues.reduce((acc, x) => acc + possible[x % possible.length], "");
   const code_verifier = randomString;
   const data = new TextEncoder().encode(code_verifier);
-  const hashed = await crypto.subtle.digest('SHA-256', data);
+  const hashed = await window.crypto.subtle.digest('SHA-256', data);
 
   const code_challenge_base64 = btoa(String.fromCharCode(...new Uint8Array(hashed)))
     .replace(/=/g, '')
